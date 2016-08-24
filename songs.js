496b6cc870908c23fdd92cf8fc5c2f21ec666f30
Meg Ducharme <ducharme.meg@gmail.com>
2016-08-23 21:19:11 -0500

    imported firebase data

--- songs.js
+++ songs.js
@@ -1,5 +1,7 @@
 console.log("hi")
 
+////////// XMLHttpRequest for JSON song file ///////////
+
 var moreSongs = (function () {
   var loadSongs = [];
 
@@ -19,38 +21,93 @@ var moreSongs = (function () {
         container.innerHTML += `
           <p>${songs.songName} by
           ${songs.artistName} on the album
-          ${songs.albumName}<button id="deleteButton">Delete</button></p>`
+          ${songs.albumName}
+          <button id="deleteButton">Delete</button></p>`
+        })
+    },
+     getSongs2: function (cb) {
+      var xhr = new XMLHttpRequest()
+      xhr.open('GET', 'moresongs.json')
+      xhr.addEventListener('load', function (evt) {
+        songsJSON = JSON.parse(evt.target.responseText).songs
+        cb(songsJSON)
+      })
+      xhr.send()
+    },
+    populatePage2: function () {
+      var container = document.querySelector("#songBox")
+      songsJSON.forEach(function(songs) {
+        container.innerHTML += `
+          <p>${songs.songName} by
+          ${songs.artistName} on the album
+          ${songs.albumName}
+          <button id="deleteButton">Delete</button></p>`
     })
-  }
-}
+  },
+    getSongs3: function(cb) {
+      console.log("getsongs3 is running")
+      $.ajax({
+      url: 'https://music-history-830c5.firebaseio.com/songs.json/',//<.json is important!
+    }).done(function(songData) {
+      console.dir(songData)
+      moreSongs.populatePage3(songData);
+    });
+    },
+    populatePage3: function (firebaseObject) {
+      console.log("poppage3 is running")
+      var container = document.querySelector("#songBox")
+      for(key in firebaseObject) {
+        // console.log(songs)
+        container.innerHTML += `
+          <p>${firebaseObject[key].songName} by
+          ${firebaseObject[key].artistName} on the album
+          ${firebaseObject[key].albumName}
+          <button id="deleteButton">Delete</button></p>`
+        }
+      }
+    }
 
 })(moreSongs || {})
 
 moreSongs.getSongs(moreSongs.populatePage)
+moreSongs.getSongs3(moreSongs.populatePage3)
 
-document.getElementById("songBox").addEventListener("click", function(event){
+$("#moreSongsBtn").click(function(){
+moreSongs.getSongs2(moreSongs.populatePage2)
+});
+
+////////// Usering Adding New Song ///////////
+
+
+// when user enteres song name, artist and album on the add music page this will push to the finalArray and display on the page
+$("#addMusicButton").on("click", function () {
+  addView.classList.add("hidden");
+  homeView.classList.remove("hidden");
+  var nameOfSong = $("#userInputSong").val();
+  var nameOfArtist = $("#userInputArtist").val();
+  var nameOfAlbum = $("#userInputAlbum").val();
+  var addNewSong = nameOfSong + " - by " + nameOfArtist + " on the album " + nameOfAlbum
+  // finalArray.push(addNewSong);
+  // console.log(finalArray)
+  songBox.innerHTML +=
+    `<p> ${addNewSong} <button id="deleteButton">Delete</button></p>`
+  $("#userInputSong").value = ""
+  $("#userInputArtist").value = ""
+  $("#userInputAlbum").value = ""
+})
+
+
+
+////////// Delete Button Event Listener ///////////
+
+$("#songBox").on("click", function(event){
     if(event.target.id === "deleteButton")
     event.target.parentElement.remove()
 })
 
 
 
-// when user enteres song name, artist and album on the add music page this will push to the finalArray and display on the page
-document.getElementById("addMusicButton").addEventListener("click", function () {
-  addView.classList.add("hidden");
-  homeView.classList.remove("hidden");
-  var nameOfSong = document.getElementById("userInputSong").value;
-  var nameOfArtist = document.getElementById("userInputArtist").value;
-  var nameOfAlbum = document.getElementById("userInputAlbum").value;
-  var addNewSong = nameOfSong + " - by " + nameOfArtist + " on the album " + nameOfAlbum
-  // finalArray.push(addNewSong);
-  // console.log(finalArray)
-  songBox.innerHTML +=
-    `<p> ${addNewSong} <button id="deleteButton">Delete</button></p>`
-  document.getElementById("userInputSong").value = ""
-  document.getElementById("userInputArtist").value = ""
-  document.getElementById("userInputAlbum").value = ""
-})
+////////// Music Hisotry 2 ///////////
 
 // var songs = [];
 
